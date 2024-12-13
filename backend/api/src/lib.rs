900dd72e5fab617e0e654c3259ca892b041be92d
chinxeleer <blessingeleer.programming@gmail.com>
2024-12-13 14:03:54 +0200

    initial commit

---
+++ backend/api/src/lib.rs
@@ -0,0 +1,32 @@
+use std::env;
+
+use axum::{routing::get, Router};
+use service::sea_orm::Database;
+
+#[tokio::main]
+async fn start() -> anyhow::Result<()> {
+    env::set_var("RUST_LOG", "debug");
+    dotenvy::dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let conn = Database::connect(db_url)
+        .await
+        .expect("Database connection failed");
+
+    // ----> Axum
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
+
+pub fn main() {
+    let result = start();
+
+    if let Some(err) = result.err() {
+        println!("Error: {err}");
+    }
+}
